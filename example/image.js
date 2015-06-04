b41dddc7ffc07cffc2b1a803020649624504b3e4
ReneHollander <rene.hollander@hotmail.de>
2015-06-04 21:57:27 +0200

    removed usage from examples

--- example/image.js
+++ example/image.js
@@ -1,11 +1,5 @@
 var raspberrypi = require('../lib/raspberrypi.js');
 
-/*
-Arguments:
-- Filename (only jpeg, no progressive)
-- Duration (seconds)
-- Callback when done displaying (argument: return code from openmax)
-*/
 raspberrypi.displayImage(__dirname + "/data/raspberry.jpg", 10, function(retval) {
   console.log(retval);
 });
--- example/video.js
+++ example/video.js
@@ -1,10 +1,5 @@
 var raspberrypi = require('../lib/raspberrypi.js');
 
-/*
-Arguments:
-- Filename (only raw h264 files)
-- Callback when done displaying (argument: return code from openmax)
-*/
 raspberrypi.playVideo(__dirname + "/data/bigbuckbunny.h264", function(retval) {
   console.log(retval);
 });
